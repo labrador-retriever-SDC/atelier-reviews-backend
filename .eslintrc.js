9442886bd826abc536ec289b58463ea643e4178c
Shuhua-L <sl.shuhualiu@gmail.com>
2023-05-29 15:45:44 -0400

    chore: cleap up configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
       sourceType: 'module',
       project: './tsconfig.json',
   },
-  ignorePatterns: ['dist', 'node_modules', 'dump', '*config.js', '*.test.js'],
+  ignorePatterns: ['dist', 'node_modules', 'dump', '*config.js', '*.test.js', '.eslintrc.js'],
   plugins: ['@typescript-eslint'],
   rules: {
       "import/extensions": [
